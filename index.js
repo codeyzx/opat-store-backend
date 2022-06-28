9a1fad9ac5bd615076d7061dd4b588a639c0b1c8
Yahya <yahyatruth@gmail.com>
2022-06-28 14:59:43 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBConnection Successfull!"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+const User = require("../models/User");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const salt = await bcrypt.genSalt(parseInt(process.env.PASS_SALT));
+  const hash = await bcrypt.hash(req.body.password, salt);
+
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: hash,
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+    !user && res.status(401).json("Username not found!");
+
+    const hashedPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !hashedPassword && res.status(401).json("Wrong Password!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({ others, accessToken });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+module.exports = router;
